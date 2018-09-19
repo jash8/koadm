203064a63d8583158a23eb257185045cf5eab4f5
jkerush <zzh3258@qq.com>
2018-09-19 15:08:59 +0800

    添加商品详情接口

--- routes/goods.js
+++ routes/goods.js
@@ -8,6 +8,90 @@ const CategorySubModel = require('../dataBase/schema/CategorySub')
 
 let router = new Router()
 
+// 获取单个商品接口
+
+router.post('/getGoodsList', async(ctx) => {
+  let categorySubId = ctx.request.body.categorySubId
+  let page = ctx.request.body.page
+  let num = 10
+  let start = (page-1)*num
+
+  console.log(ctx.request.body)
+  await GoodsModel
+    .find({SUB_ID:String(categorySubId)})
+    .skip(start).limit(num).exec()
+    .then(result => {
+      ctx.body = {
+        code: 200,
+        message: result
+      }
+    })
+    .catch(err=>{
+      console.log(err)
+      ctx.body = {
+        code: 500,
+        message: JOSN.stringfy(err)
+      }
+    })
+})
+
+
+
+
+// 获取商品小分类接口
+
+router.post('/getCategorySubList', async(ctx) => {
+  let categoryId = ctx.request.body.categoryId
+  console.log(ctx.request.body)
+  await CategorySubModel
+    .find({MALL_CATEGORY_ID:Number(categoryId)})
+    .exec()
+    .then(result => {
+      ctx.body = {
+        code: 200,
+        message: result
+      }
+    })
+    .catch(err=>{
+      console.log(err)
+      ctx.body = {
+        code: 500,
+        message: JOSN.stringfy(err)
+      }
+    })
+})
+
+
+
+
+// 获取商品大分类接口
+
+router.get('/getCategoryList', async(ctx) => {
+  await CategoryModel
+    .find()
+    .exec()
+    .then(result => {
+      ctx.body = {
+        code: 200,
+        message: result
+      }
+    })
+    .catch(err=>{
+      console.log(err)
+      ctx.body = {
+        code: 500,
+        message: err
+      }
+    })
+})
+
+
+
+
+
+
+
+
 // 获取商品详情信息接口
 
 router.post('/getDetailGoodsInfo', async(ctx) => {
